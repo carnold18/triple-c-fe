8187a8dd3856dfdff4a2e149e3857016d3f3ecc5
Christopher Arnold <chrisarnold2010@gmail.com>
2019-01-02 21:40:46 -0600

    added url consts for database gets

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends Component { 
+
+  state = {
+    currentUser: {},
+    token: "",
+    isLoggedIn: false,
+  }
+
   render() {
+
+    const API_KEY =`${process.env.REACT_APP_NREL_KEY}`
+    const zip_code = this.state.currentUser.zip_code
+    const USERS_URL = "https://sleepy-peak-12593.herokuapp.com/users"
+    const CITIES_URL = "https://sleepy-peak-12593.herokuapp.com/cities"
+    const NREL_URL = "https://developer.nrel.gov/api/cleap/v1/energy_cohort_data?zip="+zip_code+"&api_key="+API_KEY
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <h1>TRIPLE C</h1>
           </p>
           <a
             className="App-link"
